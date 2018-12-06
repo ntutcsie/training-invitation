03eb0bc184a4e2df6c2815131806539769086e70
DevilTea <ch870814@gmail.com>
2018-12-07 01:02:49 +0800

    add form dialog component

---
+++ src/components/form-dialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-dialog
+    v-model="visibility"
+    max-width="500px"
+  >
+    <v-btn
+      slot="activator"
+      color="primary"
+      dark
+    >現在報名</v-btn>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visibility: false
+    }
+  }
+}
+</script>
