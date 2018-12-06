69df3eeb0923fd8cf452a24b9beed53b498a5e8d
DevilTea <ch870814@gmail.com>
2018-12-06 21:23:42 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-content app>
 
     </v-content>
-    <v-footer app>
-
-    </v-footer>
+    <v-footer dark height="auto">
+      <v-card class="flex" flat tile>
+        <v-card-actions
+          class="justify-center"
+        >
+          © NTUT CSIE Student Association {{ getYear() }}
+        </v-card-actions>
+      </v-card>
+</v-footer>
   </v-app>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    getYear () {
+      return (new Date()).getFullYear()
+    }
+  }
+}
+</script>
